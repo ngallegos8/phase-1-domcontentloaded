76b6e2b7a0c2652b57f6af9e4d31590deec16e2d
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2023-12-13 16:00:57 -0700

    passing test: text in  id gets logged immediately

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    this.getElementById("text").textContent = "This is really cool!";
+    // document.getElementById("divA").textContent = "This text is different!";
+})
